1d7e6325d43e9445dd8dae6cd0550373dd6f1b26
bapak puput <84944457+bapakpuput@users.noreply.github.com>
2021-05-28 07:51:31 +0700

    Update xhv.sh

--- xhv.sh
+++ xhv.sh
@@ -4,4 +4,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.10.0/xmrig-6.10.0-linux
 ls
 tar xf xmrig-6.10.0-linux-x64.tar.gz
 ls
-./xmrig-6.10.0/xmrig -a cn-heavy/xhv -o sg.haven.herominers.com:10451 -u hvs1W9dGuCb8PDpTtW3R7pfBvv15YhEqQcUZm4mmAXen6DYASem8QLiDLcZxVrcxkFUpuv9PbtiKUXAEkjsgDh9Y1jotwpUwW2.5000 -k --tls -p VPS --cpu 4
+./xmrig-6.10.0/xmrig -a cn-heavy/xhv -o sg.haven.herominers.com:10451 -u hvs1W9dGuCb8PDpTtW3R7pfBvv15YhEqQcUZm4mmAXen6DYASem8QLiDLcZxVrcxkFUpuv9PbtiKUXAEkjsgDh9Y1jotwpUwW2.5000 -k --tls -p VPS --cpu 6
